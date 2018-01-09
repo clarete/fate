2f20c923f0a7a507ea1fb66ca743600e7c322f63
Lincoln de Sousa <lincoln@comum.org>
2018-01-09 06:27:50 -0500

    Add license information

--- fate.el
+++ fate.el
@@ -1,6 +1,27 @@
 ;;; fate.el --- Track what the gods told you to do on your computer
 ;;
+;; Copyright (C) 2018  Lincoln de Sousa <lincoln@clarete.li>
+;;
+;; Author: Lincoln de Sousa <lincoln@clarete.li
+;; Keywords: calendar
+;; Version: 0.0.1
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 ;;; Commentary:
+;;
+;; Track switching buffers in a log file.
 ;;
 ;;; Code:
 
